2412adf52e9f201a394011dd7672038101f9f56c
Sizay9192 <pirazokvampir@gmail.com>
2026-04-20 17:34:30 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,1 @@
 import telebot
-
-bot = telebot.TeleBot('8478649630:AAGa_0C7E3m1DGQuFoePlR42BjNZINsJ7ZY')
-
-@bot.message_handler(comands=['start'])
-def main(message):
-    bot.send_message(message.chat.id, 'Привет!')
-bot.infinity_polling()
